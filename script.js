ecf90c6e8560db7cebc0aa3a76bee35beef93728
AlbertoWebDev <albertovazquezmonge@gmail.com>
2026-01-14 23:19:21 +0100

    Background change

--- script.js
+++ script.js
@@ -3,6 +3,6 @@
 const videoFondo = document.getElementById('backgroundVideo');
 videoFondo.addEventListener('ended', function() {
     // Definimos el segundo donde se va a empezar el bucle del video
-    videoFondo.currentTime = 15; 
+    videoFondo.currentTime = 16; 
     videoFondo.play();
 });
